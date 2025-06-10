0b06191d537a5829e290ab3512536a400d8b512d
Documenter.jl <documenter@juliadocs.github.io>
2025-06-10 15:52:21 +0000

    build based on 0e85841

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Yagami","page":"Home","title":"Yagami","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Yagami.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
