900b9a7f947cbf2b20fe9fad1cda2b6aaf0767a9
Marco <m1menari@eng.ucsd.edu>
2025-06-16 16:28:15 +0200

    Delete src/curtisgodson directory
    need to change the name

--- src/curtisgodson/CurtisGodson.jl
+++
@@ -1,7 +0,0 @@
-module CurtisGodson
-  using DocStringExtensions
-  using LinearAlgebra,StructArrays,FastGaussQuadrature
-  include("quadraturepoints.jl")
-  export QuadraturePoints, get_quadrature_points, linintegral
-
-end
